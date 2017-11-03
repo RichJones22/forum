8a38e6e8df70dfd09c6fe55742ba3549084f0db7
Rich Jones <jones_rich@yahoo.com>
2017-11-03 06:28:07 -0400

    added barryvdh ide_helper

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/threads', 'ThreadsController@index');
+Route::get('/home',             'HomeController@index')
+    ->name('home');
+
+// threads model
+Route::get('/threads',          'ThreadsController@index');
 Route::get('/threads/{thread}', 'ThreadsController@show');
